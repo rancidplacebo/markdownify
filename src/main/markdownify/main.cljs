e24988a98288995e3a836acf230b05794b77d213
Scott Wagner <wagnermatthew@gmail.com>
2021-10-25 21:36:54 -0400

    adding flash message when clicking the copy to clipboard buttons

--- src/main/markdownify/main.cljs
+++ src/main/markdownify/main.cljs
@@ -4,10 +4,25 @@
             ["showdown" :as showdown]))
 
 (defonce markdown (r/atom ""))
+(defonce html (r/atom ""))
+
+(defonce flash-message (r/atom nil))
+(defonce flash-timeout (r/atom nil))
+
 (defonce showdown-converter (showdown/Converter.))
+
+(defn flash
+  ([text]
+   (flash text 3000))
+  ([text ms]
+   (reset! flash-message text)
+   (reset! flash-timeout (js/setTimeout #(reset! flash-message nil) ms))))
 
 (defn md->html [md]
   (.makeHtml showdown-converter md))
+
+(defn html->md [html]
+  (.makeMarkdown showdown-converter html)  )
 
 ;;https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697fc
 (defn copy-to-clipboard [s]
@@ -27,59 +42,90 @@
       (-> js/document .getSelection (.addRange selected)))))
 
 (defn app []
-  [:h1 "Markdownify"]
-  [:div
-    {:style {:display :flex}}
+[:div {:style {:position :relative}}
+   [:div {:style {:position :absolute
+                  :margin :auto
+                  :left 0
+                  :right 0
+                  :text-align :center
+                  :max-width 200
+                  :padding "2em"
+                  :background-color "yellow"
+                  :z-index 100
+                  :border-radius 20
+                  :transform (if @flash-message
+                               "scaleY(1)"
+                               "scaleY(0)")
+                  :transition "transform 0.2s ease-out"}} 
+    @flash-message]  
+   [:h1 "Markdownify"]
+   [:div {:style {:display :flex}}
     [:div
-      {:style {:flex "1"}}
-      [:h2 "Markdown"]
-      [:textarea
-        {:on-change #(reset! markdown (-> % .-target .-value))
-         :value @markdown
-         :style {:resize "none"
-                 :height "200px"
-                 :width "100%"}}]
-      [:button
-       {:on-click #(copy-to-clipboard @markdown)
-        :style {:background-color :green
-                :margin-top "1em"
-                :padding "0.7em"
-                :color :white
-                :border-radius 10}}
-       "Copy Markdown"]]
-    
+     {:style {:flex "1"}}
+     [:h2 "Markdown"]
+     [:textarea
+      {:on-change (fn [e]
+                    (reset! markdown (-> e .-target .-value))
+                    (reset! html (md->html (-> e .-target .-value))))
+       :value @markdown
+       :style {:resize "none"
+               :height "200px"
+               :width "100%"}}]
+     [:button
+      {:on-click (fn [] 
+                   (copy-to-clipboard @markdown)
+                   (flash "Markdown copied to clipboard"))
+       :style {:background-color :green
+               :margin-top "1em"
+               :padding "0.7em"
+               :color :white
+               :border-radius 10}}
+      "Copy Markdown"]]
+
     [:div
-      {:style {:flex "1"
-               :padding-left "2em"}}
-      [:h2 "HTML Preview"]
-      [:div 
-       {:style {:height "200px"}
-        :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]
-       [:button
-        {:on-click #(copy-to-clipboard (md->html @markdown))
-         :style {:background-color :green
-                 :margin-top "1em"
-                 :padding "0.7em"
-                 :color :white
-                 :border-radius 10}}
-        "Copy HTML"]]])
+     {:style {:flex "1"
+              :padding-left "2 em"}}
+     [:h2 "HTML"]
+     [:textarea
+      {:on-change (fn [e]
+                    (reset! markdown (-> e .-target .-value))
+                    (reset! html (html->md (-> e .-target .-value))))
+       :value @html
+       :style {:resize "none"
+               :height "200px"
+               :width "100%"}}]
+     [:button
+       {:on-click (fn []
+                   (copy-to-clipboard @markdown)
+                   (flash "HTML copied to clipboard"))
+       :style {:background-color :green
+               :margin-top "1em"
+               :padding "0.7em"
+               :color :white
+               :border-radius 10}}
+      "Copy HTML"]]
 
-
-
-
-
-
-
-
-
-
-
-
-
+    [:div
+     {:style {:flex "1"
+              :padding-left "2em"}}
+     [:h2 "HTML Preview"]
+     [:div
+      {:style {:height "200px"}
+       :dangerouslySetInnerHTML {:__html @html}}]
+     [:button
+       {:on-click (fn []
+                   (copy-to-clipboard @markdown)
+                   (flash "HTML copied to clipboard"))
+       :style {:background-color :green
+               :margin-top "1em"
+               :padding "0.7em"
+               :color :white
+               :border-radius 10}}
+      "Copy HTML"]]]])
 
 (defn mount! []
   (rdom/render [app]
-                  (.getElementById js/document "app")))
+    (.getElementById js/document "app")))
 
 (defn main! []
   (mount!))
